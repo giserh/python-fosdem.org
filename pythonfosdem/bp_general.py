ed5fd952ef696372120a3b6b53324f2025ce0093
Christophe Simonis <christophe@kn.gl>
2012-12-12 01:23:18 +0100

    [IMP] show flash messages

--- pythonfosdem/bp_general.py
+++ pythonfosdem/bp_general.py
@@ -10,9 +10,11 @@
 """
 from flask import render_template
 from flask import Blueprint
+from flask import flash
 from flask import redirect
 
 from flask import url_for
+from flask.ext.babel import gettext as _
 
 from pythonfosdem.extensions import db
 # from pythonfosdem.models import Speaker
@@ -52,6 +54,7 @@ def talk_proposal():
         db.session.add(talkProposal)
         db.session.commit()
 
+        flash(_('Thank you for you proposal'), 'info')
         return to_index()
     return render_template('general/talk_proposal.html', form=form)
 
