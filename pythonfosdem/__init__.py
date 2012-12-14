9b063b83c7b75813218a5c1edf6110e84d9c2486
Stephane Wirtel <stephane@wirtel.be>
2012-12-14 02:02:46 +0100

    [IMP] add an errorhandler for the 404 and 500

--- pythonfosdem/__init__.py
+++ pythonfosdem/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
+
 from flask import Flask
+from flask import render_template
+
+from flask.ext.babel import _
+from flask.ext.mail import Message
 
 from pythonfosdem.bp_general import blueprint as bp_general
 from pythonfosdem.config import DefaultConfig
@@ -27,6 +32,7 @@ class App(Flask):
 
         self.configure_blueprints()
         self.configure_extensions()
+        self.configure_error_handlers()
 
     def configure_blueprints(self):
         self.register_blueprint(bp_general)
@@ -36,6 +42,21 @@ class App(Flask):
         babel.init_app(self)
         mail.init_app(self)
         db.init_app(self)
+
+    def configure_error_handlers(self):
+        @self.errorhandler(404)
+        def not_found(error):
+            return render_template('errors/404.html'), 404
+
+        @self.errorhandler(500)
+        def internal_error(error):
+            message = Message(_('[PythonFOSDEM] Error 500'),
+                              sender='internal-error@python-fosdem.org',
+                              recipients=['stephane@wirtel.be'])
+            message.body = render_template('emails/send_error_500.txt', error=error)
+
+            mail.send(message)
+            return render_template('errors/500.html'), 500
 
 
 def create_app(config=None):
