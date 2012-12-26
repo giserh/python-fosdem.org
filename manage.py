dea5c63cec1001e25499176954a99b76ebe677ee
Stephane Wirtel <stephane@wirtel.be>
2012-12-26 18:55:09 +0100

    [IMP] add a new role and add new users to this role

--- manage.py
+++ manage.py
@@ -14,6 +14,7 @@ from pythonfosdem import create_app
 from pythonfosdem.extensions import db
 from pythonfosdem.models import user_datastore
 
+
 def main():
     manager = Manager(create_app)
 
@@ -21,22 +22,39 @@ def main():
     def db_populate():
         admin = user_datastore.create_role(name='admin')
         speaker = user_datastore.create_role(name='speaker')     # noqa
+        jury_member = user_datastore.create_role(name='jury_member')
 
         stephane = user_datastore.create_user(name=u'Stéphane Wirtel',
                                               password='1234',
                                               email='stephane@wirtel.be',
                                               active=True,
                                               )
+
         christophe = user_datastore.create_user(name='Christophe Simonis',
                                                 password='1234',
                                                 email='christophe@simonis.kn.gl',
                                                 active=True,
                                                 )
 
+        tarek = user_datastore.create_user(name='Tarek Ziade',
+                                           password='1234',
+                                           email='tarek@ziade.org',
+                                           active=True)
+
+        ludovic = user_datastore.create_user(name='Ludovic Gasc',
+                                             password='1234',
+                                             email='gmludo@gmail.com',
+                                             active=True)
+
         # TODO create Ludo and Tarek
 
         user_datastore.add_role_to_user(stephane, admin)
         user_datastore.add_role_to_user(christophe, admin)
+
+        user_datastore.add_role_to_user(stephane, jury_member)
+        user_datastore.add_role_to_user(christophe, jury_member)
+        user_datastore.add_role_to_user(tarek, jury_member)
+        user_datastore.add_role_to_user(ludovic, jury_member)
 
         db.session.commit()
 
