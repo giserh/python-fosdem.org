5aa92fc02fb2109bd2c162cdc3cd5b7690742d58
Stephane Wirtel <stephane@wirtel.be>
2012-12-29 01:47:23 +0100

    [IMP] sort the talk proposals by the name of the user

--- pythonfosdem/bp_general.py
+++ pythonfosdem/bp_general.py
@@ -27,6 +27,7 @@ from pythonfosdem.extensions import mail
 from pythonfosdem.models import Talk
 from pythonfosdem.models import TalkProposal
 from pythonfosdem.models import TalkVote
+from pythonfosdem.models import User
 from pythonfosdem.forms import TalkProposalForm
 from pythonfosdem.forms import UserProfileForm
 
@@ -108,7 +109,7 @@ def open_talk_proposal():
 @blueprint.route('/talk_proposals')
 @roles_accepted('admin', 'jury_member')
 def talk_proposals():
-    records = Talk.query.filter_by(state='draft').all()
+    records = Talk.query.filter_by(state='draft').join(Talk.user).order_by(User.name.asc()).all()
     # records = TalkProposal.query.all()
     return render_template('general/talk_proposals.html', records=records)
 
